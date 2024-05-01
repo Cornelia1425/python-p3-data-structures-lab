3e0ee10e4f3f930bc8d33770a0adad8ffe589b0c
yiranshu <shuyiran1425@hotmail.com>
2024-05-01 16:13:15 -0400

    list

--- lib/data_structures.py
+++ lib/data_structures.py
@@ -17,22 +17,54 @@ spicy_foods = [
 ]
 
 def get_names(spicy_foods):
-    pass
+    names =[food["name"] for food in spicy_foods]
+    return names
 
 def get_spiciest_foods(spicy_foods):
-    pass
+    # spiciest_food=spicy_foods[0]
+    # for food in spicy_foods:
+    #     print(food["heat_level"])
+    #     if food["heat_level"] > spiciest_food["heat_level"]:
+    #         spiciest_food = food
+    # elif:
+    #     do nothing
+
+    # spiciest_food=[]
+    # for food in spicy_foods:
+    #     if food["heat_level"]>5:
+    #         spiciest_food.append(food)
+
+    spiciest_food = [food for food in spicy_foods if food["heat_level"]>5]
+    return spiciest_food
 
 def print_spicy_foods(spicy_foods):
-    pass
+    for food in spicy_foods:
+        print ( food["name"] + " (" + food["cuisine"] + ") | Heat Level: " + "🌶"* food["heat_level"])
 
 def get_spicy_food_by_cuisine(spicy_foods, cuisine):
-    pass
+    # spicy_food_by_cuisine = ["cuisine: " + food["cuisine"] + "heat_level: " + food["heat_level"] + "name: " + food["name"] for food in spicy_foods if food["cuisine"] == cuisine]
+    # return spicy_food_by_cuisine
+
+    for food in spicy_foods:
+        if food["cuisine"] == cuisine:
+            return food
+
+
+    # for food in spicy_foods:
+    #     print ("cuisine: " + food["cuisine"] + "heat_level: " + food["heat_level"] + "name: " + food["name"])
+
 
 def print_spiciest_foods(spicy_foods):
-    pass
+       for food in spicy_foods:
+            if food["heat_level"]>5:
+                print ( food["name"] + " (" + food["cuisine"] + ") | Heat Level: " + "🌶"* food["heat_level"])
 
 def get_average_heat_level(spicy_foods):
-    pass
+    heat_level_list = [food["heat_level"] for food in spicy_foods]
+    get_average_heat_level = sum(heat_level_list)/len(heat_level_list)
+    return get_average_heat_level
 
 def create_spicy_food(spicy_foods, spicy_food):
-    pass
+    spicy_foods.append(spicy_food)
+    return spicy_foods
+
